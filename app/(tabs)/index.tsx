0ccb53613c97483ffbddbd1d3bd8e1804f246bbd
Susant Paudel <techbizznepal@gmail.com>
2024-12-09 04:15:30 +0545

    component composition

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,26 @@
 import Header from "@/components/Header";
+import React from "react";
+import {Platform, ScrollView, Text} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+import DeliveryBar from "@/components/DeliveryBar";
+
 
 export default function HomeScreen() {
-    return <Header/>
+    const [sliders,] = React.useState([1, 2, 3])
+    const ios = Platform.OS === 'ios';
+    return (
+        <SafeAreaView className={`flex-1 bg-orange-800` + (ios ? ' pb-2' : ' pt-3')}>
+            <Header/>
+            <DeliveryBar/>
+            <ScrollView
+                className="bg-white px-2"
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{paddingBottom: 10}}
+            >
+                {sliders.map((i) => (
+                    <Text key={i} className="text-black mb-4">Hello World</Text>
+                ))}
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
