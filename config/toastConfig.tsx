a2ceffc66b4142ff5a93275fc5d4e42a93679f01
Susant Paudel <techbizznepal@gmail.com>
2025-01-25 23:47:12 +0545

    add toast on profile update

--- config/toastConfig.tsx
+++ config/toastConfig.tsx
@@ -1,24 +1,36 @@
 import { BaseToast, ErrorToast, ToastProps } from "react-native-toast-message";
+import { Colors } from "@/constants/Colors";
 
 const toastConfig = {
   success: (props: ToastProps) => (
     <BaseToast
       {...props}
-      contentContainerStyle={{ paddingHorizontal: 15 }}
+      contentContainerStyle={{
+        paddingHorizontal: 10,
+        backgroundColor: Colors.light.tint,
+        width: "auto",
+        borderLeftColor: "yellow",
+      }}
       text1Style={{
         fontSize: 15,
         fontWeight: "400",
+        color: "white",
       }}
     />
   ),
   error: (props: ToastProps) => (
     <ErrorToast
       {...props}
+      contentContainerStyle={{
+        backgroundColor: "red",
+      }}
       text1Style={{
         fontSize: 17,
+        color: "white",
       }}
       text2Style={{
         fontSize: 15,
+        color: "white",
       }}
     />
   ),
