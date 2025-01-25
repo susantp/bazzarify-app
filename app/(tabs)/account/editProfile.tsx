ef279f2ffece57ae29322234aed7ae99b5b25ca2
Susant Paudel <techbizznepal@gmail.com>
2025-01-26 02:32:14 +0545

    text input v1 refactor

--- app/(tabs)/account/editProfile.tsx
+++ app/(tabs)/account/editProfile.tsx
@@ -2,8 +2,6 @@ import {
   Image,
   ScrollView,
   Text,
-  TextInput,
-  TextInputProps,
   TouchableOpacity,
   useWindowDimensions,
   View,
@@ -16,6 +14,7 @@ import { Colors } from "@/constants/Colors";
 import { useRecoilValue } from "recoil";
 import { userProfileAtom } from "@/atoms/sessionAtom";
 import Toast from "react-native-toast-message";
+import TextInputV1 from "@/components/common/TextInputV1";
 
 export default function Page() {
   const { width } = useWindowDimensions();
@@ -31,64 +30,58 @@ export default function Page() {
     <SafeAreaWrapper>
       <ScreenHeader title="Edit Profile" />
       <ScrollView className="flex-1 bg-white">
-        <ContentWrapper className="items-center gap-y-4 px-4 py-3.5">
-          <View>
-            <Image
-              source={require("@/assets/images/profile.png")}
-              style={{ width: 100, height: 100 }}
-            />
-            <View className="absolute bottom-0 right-0">
-              <AntDesign
-                name="pluscircle"
-                size={24}
-                color={Colors.light.tint}
+        <ContentWrapper className="gap-y-4 px-4 py-3.5">
+          <View className="flex items-center">
+            <View>
+              <Image
+                source={require("@/assets/images/profile.png")}
+                style={{ width: 150, height: 150 }}
               />
+              <TouchableOpacity
+                activeOpacity={0.4}
+                className="absolute bottom-2 right-2"
+              >
+                <AntDesign
+                  name="pluscircle"
+                  size={24}
+                  color={Colors.light.tint}
+                />
+              </TouchableOpacity>
             </View>
           </View>
           <TextInputV1
-            style={{ width: width * 0.95 }}
             defaultValue={userProfile.name}
             placeholder={"Enter Your Profile"}
           />
           <TextInputV1
-            style={{ width: width * 0.95 }}
             defaultValue={userProfile.displayName}
             placeholder={"Display name (opt)"}
           />
           <TextInputV1
             keyboardType={"numeric"}
-            style={{ width: width * 0.95 }}
             defaultValue={userProfile.phoneNumber}
             placeholder={"phone"}
           />
           <TextInputV1
             keyboardType={"email-address"}
-            style={{ width: width * 0.95 }}
             defaultValue={userProfile.email}
             placeholder={"email"}
           />
-          <TextInputV1
-            style={{ width: width * 0.95 }}
-            defaultValue={userProfile.dob}
-            placeholder={"dob"}
-          />
+          <TextInputV1 defaultValue={userProfile.dob} placeholder={"dob"} />
           <View className="w-full items-start pt-3">
             <Text className="text-lg font-semibold">Update Password</Text>
           </View>
 
           <TextInputV1
             keyboardType="visible-password"
-            style={{ width: width * 0.95 }}
             placeholder={"Current Password"}
           />
           <TextInputV1
             keyboardType="visible-password"
-            style={{ width: width * 0.95 }}
             placeholder={"New Password"}
           />
           <TextInputV1
             keyboardType="visible-password"
-            style={{ width: width * 0.95 }}
             placeholder={"Confirm Password"}
           />
           <TouchableOpacity
@@ -103,12 +96,3 @@ export default function Page() {
     </SafeAreaWrapper>
   );
 }
-
-const TextInputV1 = (props: TextInputProps) => {
-  return (
-    <TextInput
-      className="space-x-3 rounded-xl border border-slate-300 bg-white py-3.5"
-      {...props}
-    />
-  );
-};
---
+++ components/common/TextInputV1.tsx
@@ -0,0 +1,21 @@
+import { Text, TextInput, TextInputProps, View } from "react-native";
+
+interface TextInputV1Props extends TextInputProps {
+  legend?: string;
+}
+
+const TextInputV1 = (props: TextInputV1Props) => {
+  return (
+    <View>
+      {props.legend && (
+        <View className="py-2">
+          <Text className="font-semibold">{props.legend}</Text>
+        </View>
+      )}
+      <View className="flex-col gap-y-2 rounded-xl border border-slate-300 px-1">
+        <TextInput className="px-2" {...props} />
+      </View>
+    </View>
+  );
+};
+export default TextInputV1;
