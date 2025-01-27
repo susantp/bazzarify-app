066ab169cbde83a862c3df95b59ccb63c26e4dfe
Susant Paudel <techbizznepal@gmail.com>
2025-01-28 01:39:24 +0545

    wip vendor index page

---
+++ app/(tabs)/vendor/_layout.tsx
@@ -0,0 +1,69 @@
+import { Tabs } from "expo-router";
+import { HomeIcon } from "react-native-heroicons/solid";
+import React from "react";
+import { Colors } from "@/constants/Colors";
+import { HapticTab } from "@/components/HapticTab";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Platform } from "react-native";
+import { Fontisto, Ionicons } from "@expo/vector-icons";
+
+export default function Layout() {
+  return (
+    <Tabs
+      initialRouteName="index"
+      backBehavior="history"
+      screenOptions={{
+        tabBarInactiveBackgroundColor: "#fff",
+        tabBarActiveTintColor: Colors.light.tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarActiveBackgroundColor: "#fff",
+        tabBarIconStyle: {
+          color: Colors.light.icon,
+        },
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          href: null,
+          title: "Home",
+          tabBarIcon: ({ color }) => <HomeIcon size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="[id]/index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => <HomeIcon size={28} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="[id]/categories"
+        options={{
+          title: "Categories",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="grid" size={28} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="[id]/products"
+        options={{
+          title: "Categories",
+          tabBarIcon: ({ color }) => (
+            <Fontisto name="shopping-package" size={28} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
