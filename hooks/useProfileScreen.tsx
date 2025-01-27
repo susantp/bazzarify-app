1ca0a705afb9703d9e4b605aa079403ea72ee637
Susant Paudel <techbizznepal@gmail.com>
2025-01-28 04:18:48 +0545

    add menus on profile

--- hooks/useProfileScreen.tsx
+++ hooks/useProfileScreen.tsx
@@ -7,14 +7,16 @@ import {
   ToShipIcon,
 } from "@/components/common/icons";
 import React from "react";
+import { FontAwesome6 } from "@expo/vector-icons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
-export type OrderStatusBoxType = {
+export type ProfileMenuBoxType = {
   label: string;
   id: string;
   icon: React.ReactNode;
 };
 export default function useProfileScreen() {
-  const orderStatusBoxes: OrderStatusBoxType[] = [
+  const orderStatusBoxes: ProfileMenuBoxType[] = [
     {
       id: "toPay",
       label: "To Pay",
@@ -41,6 +43,19 @@ export default function useProfileScreen() {
       icon: <ToReturnIcon />,
     },
   ];
-
-  return { orderStatusBoxes };
+  const otherMenus: ProfileMenuBoxType[] = [
+    {
+      id: "toMessage",
+      label: "To Message",
+      icon: <FontAwesome6 name="inbox" size={26} color={Colors.light.tint} />,
+    },
+    {
+      id: "vouchers",
+      label: "Collect Vouchers",
+      icon: (
+        <MaterialIcons name="discount" size={26} color={Colors.light.tint} />
+      ),
+    },
+  ];
+  return { orderStatusBoxes, otherMenus };
 }
