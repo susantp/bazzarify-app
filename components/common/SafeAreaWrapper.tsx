15d21f029a3fde1396e8d24b9bdc78a27b51cdaf
Susant Paudel <techbizznepal@gmail.com>
2025-02-02 06:37:32 +0545

    add flash deal screen

--- components/common/SafeAreaWrapper.tsx
+++ components/common/SafeAreaWrapper.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import cn from "@/utils/tailwindHelper";
 
 interface SafeAreaWrapperProps {
   children: React.ReactNode[] | React.ReactNode;
+  className?: string;
 }
 
-export const SafeAreaWrapper = ({ children }: SafeAreaWrapperProps) => {
+export const SafeAreaWrapper = ({
+  children,
+  className,
+}: SafeAreaWrapperProps) => {
   return (
     <SafeAreaProvider>
-      <SafeAreaView className={`flex-col bg-orange-600`} style={{ flex: 1 }}>
+      <SafeAreaView
+        className={cn("flex-col", "bg-orange-600", className)}
+        style={{ flex: 1 }}
+      >
         {children}
       </SafeAreaView>
     </SafeAreaProvider>
