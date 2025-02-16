d349a61bfb6f2be449afeaf92582e02729cf8329
Susant Paudel <techbizznepal@gmail.com>
2025-02-16 08:28:54 +0545

    add react hook form

--- app/(tabs)/auth/login.tsx
+++ app/(tabs)/auth/login.tsx
@@ -1,45 +1,83 @@
-import {
-  Platform,
-  ScrollView,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import UsernameInput from "@/components/account/UsernameInput";
-import UserPasswordInput from "@/components/account/UserPasswordInput";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 import FullWidthActionBtn from "@/components/account/FullWidthActionBtn";
 import SocialLoginButton from "@/components/account/SocialLoginButton";
-import React from "react";
+import React, { useState } from "react";
 import PageTitle from "@/components/account/PageTitle";
 import { useRecoilState } from "recoil";
 import { userSession } from "@/atoms/sessionAtom";
 import { Link } from "expo-router";
 import LoginFormHelperText from "@/components/account/LoginFormHelperText";
 import { SafeAreaWrapper } from "@/components/common/SafeAreaWrapper";
+import { Controller, useForm } from "react-hook-form";
+import UsernameInput from "@/components/account/UsernameInput";
+import UserPasswordInput from "@/components/account/UserPasswordInput";
 
+type FormData = {
+  username: string;
+  password: string;
+};
 const LoginScreen = () => {
-  const ios: boolean = Platform.OS === "ios";
-  const inputPadding: string = ios ? "py-5" : "py-4";
   const [session, setSession] = useRecoilState(userSession);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+  const handleLogin = () => setSession(!session);
+  const [showPassword, setShowPassword] = useState(true);
   return (
     <SafeAreaWrapper>
       <ScrollView>
         <View className="h-screen-safe w-screen flex-col items-center justify-center gap-y-4 bg-gray-100">
           <PageTitle title="Login" />
           <View className="h-5" />
-          <UsernameInput
-            inputPadding={inputPadding}
-            defaultValue={`om.prakash@gmail.com`}
-          />
-          <UserPasswordInput
-            placeholder="Password"
-            inputPadding={inputPadding}
-            defaultValue={`password123`}
-          />
+
+          <View className="w-full flex-col gap-y-2 px-6">
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <UsernameInput
+                  hasError={errors.username}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                />
+              )}
+              name="username"
+            />
+          </View>
+          <View className="w-full flex-col gap-y-2 px-6">
+            <Controller
+              control={control}
+              rules={{
+                required: true,
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <UserPasswordInput
+                  hasError={errors.password}
+                  setShowPassword={setShowPassword}
+                  showPassword={showPassword}
+                  value={value}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                />
+              )}
+              name="password"
+            />
+          </View>
+
           <LoginFormHelperText />
           <View className="h-4" />
           <FullWidthActionBtn
-            handleOnPress={() => setSession(!session)}
+            handleOnPress={handleSubmit(handleLogin)}
             label="Login"
           />
           <Text className="text-gray-400">or</Text>
--- components/account/UsernameInput.tsx
+++ components/account/UsernameInput.tsx
@@ -1,23 +1,22 @@
-import { TextInput, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { EnvelopeIcon } from "react-native-heroicons/outline";
+import React from "react";
+import { InputProps } from "@/components/common";
 
-const UsernameInput = ({
-  inputPadding,
-  defaultValue,
-}: {
-  inputPadding: string;
-  defaultValue: string;
-}) => (
-  <View className="relative w-full px-6">
+const UsernameInput = ({ value, onBlur, onChange, hasError }: InputProps) => (
+  <>
     <TextInput
-      defaultValue={defaultValue}
+      value={value}
+      onBlur={onBlur}
+      onChangeText={onChange}
       placeholder="Your email/number"
-      className={`rounded-md bg-white pl-14 ${inputPadding}`}
+      className={`rounded-md bg-white pl-14`}
     />
     <View className="absolute inset-x-9 inset-y-3">
       <EnvelopeIcon size={28} strokeWidth={1} color="gray" />
     </View>
-  </View>
+    {hasError && <Text className="text-red-600">This is required.</Text>}
+  </>
 );
 
 export default UsernameInput;
