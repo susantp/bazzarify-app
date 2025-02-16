bafd913a5511dfd75d38fbdb4917af8e00133a06
Susant Paudel <techbizznepal@gmail.com>
2025-02-16 18:58:27 +0545

    add guest home page

--- app/(tabs)/guest/_layout.tsx
+++ app/(tabs)/guest/_layout.tsx
@@ -1,18 +1,18 @@
 import { useRecoilValue } from "recoil";
 import { userSession } from "@/atoms/sessionAtom";
+import Page from "@/app/(tabs)/account/profile";
 import { Stack } from "expo-router";
-import Profile from "@/app/(tabs)/account/profile";
 
-const Home = () => {
+const Layout = () => {
   const session = useRecoilValue(userSession);
-  if (session) return <Profile />;
-
+  if (session) return <Page />;
   return (
     <Stack>
+      <Stack.Screen name="guestAccountIndex" options={{ headerShown: false }} />
       <Stack.Screen name="login" options={{ headerShown: false }} />
       <Stack.Screen name="register" options={{ headerShown: false }} />
     </Stack>
   );
 };
 
-export default Home;
+export default Layout;
