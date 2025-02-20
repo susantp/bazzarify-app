0c07224479c6e123e70efc2edb0bde548b0b9036
Susant Paudel <techbizznepal@gmail.com>
2025-02-20 23:03:09 +0545

    some refactor

---
+++ utils/axios.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "http://192.168.1.64/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+export default axiosInstance;
