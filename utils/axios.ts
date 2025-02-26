06b128830ddf9c343e779b3f067f2493bbe5b156
Susant Paudel <techbizznepal@gmail.com>
2025-02-27 03:51:41 +0545

    add sentry and other network related refactor

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const hostPath = {
-  remote: "http://bazzarify.com/api/v1",
+  remote: "http://bazzarify.com/api/v1", // remove android:usesCleartextTraffic="true" after ssl activated for the domain
   local: "http://192.168.1.64/api/v1",
 };
 const axiosInstance = axios.create({
