659b2a99d91a43b52e73109995484fde6e5367b2
Susant Paudel <techbizznepal@gmail.com>
2025-02-27 04:54:29 +0545

    add free delivery banner and other input refactor

--- components/account/NameInput.tsx
+++ components/account/NameInput.tsx
@@ -19,9 +19,9 @@ const NameInput = ({
       defaultValue={rest.defaultValue}
       onChangeText={onChange}
       placeholder="Your name"
-      className={cn("rounded-md", "bg-white", "pl-14", className)}
+      className={cn("rounded-xl", "bg-white", "pl-14", className)}
     />
-    <View className="absolute inset-x-9 inset-y-2.5">
+    <View className="absolute inset-x-9 inset-y-3">
       <AntDesign name="user" size={28} strokeWidth={1} color="gray" />
     </View>
     {hasError && <Text className="text-red-600">This is required.</Text>}
