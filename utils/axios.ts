d982dc6477297bf67e003d1535549c1c0103fbbe
Susant Paudel <techbizznepal@gmail.com>
2025-02-28 01:20:39 +0545

    refactor

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 
-const hostPath = {
+const remoteData = {
   remote: "http://bazzarify.com/api/v1", // remove android:usesCleartextTraffic="true" after ssl activated for the domain
   local: "http://192.168.1.64/api/v1",
+  xAppKey: "K@thmandu#1522",
 };
 const axiosInstance = axios.create({
-  baseURL: hostPath.remote,
+  baseURL: remoteData.remote,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
+    "X-APP-KEY": remoteData.xAppKey,
   },
 });
 export default axiosInstance;
