ebf6cdbcc160eb5f1a21e2420c2d3d114bae532d
Susant Paudel <techbizznepal@gmail.com>
2025-05-20 03:24:59 +0545

    update header etc

--- modules/core/utils/axios.ts
+++ modules/core/utils/axios.ts
@@ -9,8 +9,7 @@ const remoteData: Record<string, string> = {
 const defaultConfig: CreateAxiosDefaults = {
   baseURL: remoteData.apiUrl,
   headers: {
-    "User-Agent":
-      "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Mobile Safari/537.36",
+    "User-Agent": "BazzarifyConsumer",
     "Content-Type": "application/json",
     "X-APP-Key": remoteData.appKey,
   },
