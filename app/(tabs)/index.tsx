3627a25e48ba3c0d52bb0d16fdcae088fc745d97
Susant Paudel <techbizznepal@gmail.com>
2025-12-18 00:00:10 +0545

    chore: update delivery bar color

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -46,7 +46,7 @@ export default function HomeScreen() {
           locationError={error}
           refresh={refresh}
           displayCurrentAddress={address}
-          className="flex-row items-center justify-center gap-2 bg-blue-950 py-2"
+          className="flex-row items-center justify-center gap-2 bg-orange-700 py-2"
         />
         <ContentWrapper>
           <FlatList
