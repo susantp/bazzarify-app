0bdff5784b46fb778e717e633b7ea30ed2e79a45
Susant Paudel <techbizznepal@gmail.com>
2025-12-17 23:19:20 +0545

    chore: add check update

--- scripts/android-build-steps-prod.sh
+++ scripts/android-build-steps-prod.sh
@@ -1,8 +1,8 @@
-bunx expo prebuild --clean --bun --platform android && \
-bunx expo export:embed --eager --platform android --production false && \
-pushd ./../android || exit
+bunx expo prebuild --clean --bun --platform android
+#bunx expo export:embed --eager --platform android --production false
+pushd ./android || exit
 #cd ./android && \
 #./gradlew clean && \
-./gradlew :app:assembleRelease && \
+./gradlew :app:assembleRelease
 #cd ..
 popd || exit
